d9840712a9d76b976f434c89d2f36f8c9ad1fa62
Damian Deluga <ddeluga23@gmail.com>
2023-10-28 11:59:00 +0200

    Tworzenie pustej aplikacji

---
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -0,0 +1,19 @@
+﻿string name = "Ewa";
+var gender = "kobieta";
+int age = 33;
+
+if (name == "Ewa")
+{
+    if (age == 33)
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
+    else if (age < 40 && gender == "kobieta")
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else if (age < 18 && gender != "kobieta")
+    {
+        Console.WriteLine("Niepełnoletni mężczyzna");
+    }
+}
