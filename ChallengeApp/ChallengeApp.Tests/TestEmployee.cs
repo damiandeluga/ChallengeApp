a7ad2b0f1ae8065865dda41b62a66017d44da735
Damian Deluga <ddeluga23@gmail.com>
2023-11-03 19:26:11 +0100

    Tworzenie pustego projektu

---
+++ ChallengeApp/ChallengeApp.Tests/TestEmployee.cs
@@ -0,0 +1,25 @@
+﻿namespace ChallengeApp.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void TestMinMaxAverageGradeofTheWorkerFromStatistics()
+        {
+            var employee = new Employee("Damian", "Domalewski");
+
+            employee.AddGrade(4);
+            employee.AddGrade(5);
+            employee.AddGrade(9);
+
+            var statistics = employee.GetStatistics();
+
+            Assert.AreEqual(9.0f, statistics.Max);
+            Assert.AreEqual(4.0f, statistics.Min);
+            Assert.AreEqual(6.0F, statistics.Average);
+
+
+
+        }
+
+    }
+}
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,44 +1,13 @@
 using ChallengeApp;
 
-Employee employee1 = new Employee("Adam ", "Nowak ", 25);
-Employee employee2 = new Employee("Karol ", "Kowalski ", 28);
-Employee employee3 = new Employee("Ewa ", "Karolczyk ", 30);
+var employee1 = new Employee("Adam", "Kowalski");
 
-employee1.Addscore(2);
-employee1.Addscore(4);
-employee1.Addscore(5);
-employee1.Addscore(1);
-employee1.Addscore(3);
+employee1.AddGrade(2);
+employee1.AddGrade(2);
+employee1.AddGrade(6);
 
-employee2.Addscore(6);
-employee2.Addscore(8);
-employee2.Addscore(5);
-employee2.Addscore(3);
-employee2.Addscore(1);
+var statistics = employee1.GetStatistics();
+Console.WriteLine($"Average: {statistics.Average:N2}");
+Console.WriteLine($"Max: {statistics.Max}");
+Console.WriteLine($"Min: {statistics.Min}"); 
 
-employee3.Addscore(7);
-employee3.Addscore(3);
-employee3.Addscore(4);
-employee3.Addscore(5);
-employee3.Addscore(3);
-
-List<Employee> employees = new List<Employee>()
-{
-    employee1, employee2, employee3
-};
-
-int highestResult = -1;
-Employee? employeeWithHighestResult = null;
-
-foreach (var employee in employees)
-{
-    if (employee.Result > highestResult)
-    {
-        employeeWithHighestResult = employee;
-        highestResult = employee.Result;
-    }
-
-}
-Console.WriteLine("Pracownik z najlepszym wynikiem to:" + 
-employeeWithHighestResult.Name
-+ employeeWithHighestResult.Surname + employeeWithHighestResult.Age + " lat - Ilość uzyskanych punktów to: " + employeeWithHighestResult.Result);
