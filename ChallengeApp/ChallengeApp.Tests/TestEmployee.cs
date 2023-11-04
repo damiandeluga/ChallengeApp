a105a31a69054c9d1d55a110f8c5bf5f4d9b2b59
Damian Deluga <ddeluga23@gmail.com>
2023-11-04 20:35:36 +0100

    Tworzenie pustego projektu

--- ChallengeApp/ChallengeApp.Tests/TestEmployee.cs
+++ ChallengeApp/ChallengeApp.Tests/TestEmployee.cs
@@ -3,7 +3,7 @@
     public class Tests
     {
         [Test]
-        public void TestMinMaxAverageGradeofTheWorkerFromStatistics()
+        public void TestMinGradeofTheWorkerFromStatistics()
         {
             var employee = new Employee("Damian", "Domalewski");
 
@@ -12,14 +12,39 @@
             employee.AddGrade(9);
 
             var statistics = employee.GetStatistics();
-
-            Assert.AreEqual(9.0f, statistics.Max);
+           
             Assert.AreEqual(4.0f, statistics.Min);
-            Assert.AreEqual(6.0F, statistics.Average);
+    
+        }
+        [Test]
+        public void TestMaxGradeofTheWorkerFromStatistics()
+        {
+            var employee = new Employee("Artur", "Durkowski");
+
+            employee.AddGrade(4);
+            employee.AddGrade(6);
+            employee.AddGrade(8);
+
+            var statistics = employee.GetStatistics();
+
+            Assert.AreEqual(8.0f, statistics.Max);
+      
+        }
+        [Test]
+        public void TestAverageGradeofTheWorkerFromStatistics()
+        {
+            var employee = new Employee("Cyprian", "Lizecki");
+
+            employee.AddGrade(10);
+            employee.AddGrade(15);
+            employee.AddGrade(5);
+
+            var statistics = employee.GetStatistics();
+
+            Assert.AreEqual(10.0F, statistics.Average);
 
 
 
         }
-
     }
 }
