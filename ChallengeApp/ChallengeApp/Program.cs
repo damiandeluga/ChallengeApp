b0b5203c8e066c3c87e6b8f1fae32a4f8b69dfb8
Damian Deluga <ddeluga23@gmail.com>
2023-11-06 15:38:26 +0100

    Tworzenie pustego projektu

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -17,21 +17,21 @@ Console.WriteLine($"Max: {statistics.Max}");
 Console.WriteLine($"Average: {statistics.Min:N2}");
 
 Console.WriteLine("Wyniki ocen pracowniczych dla metody GetStisticsWithForEach");
-Console.WriteLine($"Min: {statistics.Min}");
-Console.WriteLine($"Max: {statistics.Max}");
-Console.WriteLine($"Average: {statistics.Min:N2}");
+Console.WriteLine($"Min: {statistics1.Min}");
+Console.WriteLine($"Max: {statistics1.Max}");
+Console.WriteLine($"Average: {statistics1.Min:N2}");
 
 Console.WriteLine("Wyniki ocen pracowniczych dla metody GetStisticsWithDoWhile");
-Console.WriteLine($"Min: {statistics.Min}");
-Console.WriteLine($"Max: {statistics.Max}");
-Console.WriteLine($"Average: {statistics.Min:N2}");
+Console.WriteLine($"Min: {statistics2.Min}");
+Console.WriteLine($"Max: {statistics2.Max}");
+Console.WriteLine($"Average: {statistics2.Min:N2}");
 
 Console.WriteLine("Wyniki ocen pracowniczych dla metody GetStisticsWithFor");
-Console.WriteLine($"Min: {statistics.Min}");
-Console.WriteLine($"Max: {statistics.Max}");
-Console.WriteLine($"Average: {statistics.Min:N2}");
+Console.WriteLine($"Min: {statistics3.Min}");
+Console.WriteLine($"Max: {statistics3.Max}");
+Console.WriteLine($"Average: {statistics3.Min:N2}");
 
 Console.WriteLine("Wyniki ocen pracowniczych dla metody GetStisticsWithWhile");
-Console.WriteLine($"Min: {statistics.Min}");
-Console.WriteLine($"Max: {statistics.Max}");
-Console.WriteLine($"Average: {statistics.Min:N2}");
+Console.WriteLine($"Min: {statistics4.Min}");
+Console.WriteLine($"Max: {statistics4.Max}");
+Console.WriteLine($"Average: {statistics4.Min:N2}");
